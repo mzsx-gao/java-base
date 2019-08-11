924d0a91a4f024c02c8268c6656d2b7cb3c67dfb
gaoshudian <18818196720@163.com>
2019-08-11 15:56:59 +0800

    NIO部分代码提交

---
+++ annotation-processor/src/main/java/com/gao/annotation/processor/Main.java
@@ -0,0 +1,20 @@
+package com.gao.annotation.processor;
+
+/**
+ * @Description: 主类
+ *
+ * @Auther: gaoshudian
+ * @Date: 2018/12/16 21:56
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("success");
+        test();
+    }
+
+    @Test(value="method is test")
+    public static void test(){
+
+    }
+}
---
+++ other/src/main/java/com/gao/dd.java
@@ -0,0 +1,24 @@
+package com.gao;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by gao on 2018/10/14.
+ */
+public class dd {
+
+    public static void main(String[] args) {
+        Integer a = new Integer(128);
+        Integer b = 128;
+        Long c = Long.valueOf("128");
+        long d = Long.valueOf("128");
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+
+    }
+
+}
