e2903965cccccd5b50fe75a64add2a8647d9769a
gaoshudian <18818196720@163.com>
2019-12-17 22:57:11 +0800

    加入logback,log4j2日志系统模块

---
+++ log-demo/src/main/java/com/log/LogTest.java
@@ -0,0 +1,12 @@
+package com.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogTest {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(LogTest.class);
+        logger.debug("测试log日志");
+    }
+}
