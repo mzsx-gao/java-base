fd95a59d160b3c428178576fa5da45156fd7b6fd
gaoshudian <18818196720@163.com>
2020-10-27 22:41:57 +0800

    修改NIO部分代码

--- network/socket/src/main/java/com/gao/socket/io/nio/demo/Client.java
+++ network/socket/src/main/java/com/gao/socket/io/nio/demo/Client.java
@@ -9,14 +9,12 @@ public class Client {
 
 	//需要一个Selector 
 	public static void main(String[] args) {
-		
 		//创建连接的地址
 		InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8765);
 		//声明连接通道
 		SocketChannel sc = null;
 		//建立缓冲区
 		ByteBuffer buf = ByteBuffer.allocate(1024);
-		
 		try {
 			//打开通道
 			sc = SocketChannel.open();
@@ -26,7 +24,6 @@ public class Client {
 				//定义一个字节数组，然后使用系统录入功能：
 				byte[] bytes = new byte[1024];
 				System.in.read(bytes);
-				
 				//把数据放到缓冲区中
 				buf.put(bytes);
 				//对缓冲区进行复位
--- network/socket/src/main/java/com/gao/socket/io/nio/demo/Server.java
+++ network/socket/src/main/java/com/gao/socket/io/nio/demo/Server.java
@@ -14,12 +14,10 @@ public class Server implements Runnable{
 	private Selector seletor;
 	//2 建立读缓冲区
 	private ByteBuffer readBuf = ByteBuffer.allocate(1024);
-	//3 建立写缓冲区
-	private ByteBuffer writeBuf = ByteBuffer.allocate(1024);
 
 	public Server(int port){
 		try {
-			//1 打开路复用器
+			//1 打开多路复用器
 			this.seletor = Selector.open();
 			//2 打开服务器通道
 			ServerSocketChannel ssc = ServerSocketChannel.open();
@@ -27,11 +25,9 @@ public class Server implements Runnable{
 			ssc.configureBlocking(false);
 			//4 绑定地址
 			ssc.bind(new InetSocketAddress(port));
-			//5 把服务器通道注册到多路复用器上，并且监听阻塞事件
+			//5 把服务器通道注册到多路复用器上，并且"OP_ACCEPT(接收连接)"事件
 			ssc.register(this.seletor, SelectionKey.OP_ACCEPT);
-			
 			System.out.println("Server start, port :" + port);
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -41,13 +37,8 @@ public class Server implements Runnable{
 	public void run() {
 		while(true){
 			try {
-				//1 必须要让多路复用器开始监听
-                int readyChannels =this.seletor.select();
-                if(readyChannels == 0){
-                    System.out.println("。。。");
-                    continue;
-                }
-                System.out.println("readyChannels.."+readyChannels);
+				//1 获取当前有哪些事件,select()方法为阻塞方法
+                this.seletor.select();
                 //2 返回多路复用器已经选择的结果集
 				Iterator<SelectionKey> keys = this.seletor.selectedKeys().iterator();
 				//3 进行遍历
@@ -56,87 +47,71 @@ public class Server implements Runnable{
 					SelectionKey key = keys.next();
 					//5 直接从容器中移除就可以了
 					keys.remove();
-					//6 如果是有效的
+					//6.处理事件
 					if(key.isValid()){
-						//7 如果为阻塞状态
-						if(key.isAcceptable()){
-							this.accept(key);
-						}
-						//8 如果为可读状态
-						if(key.isReadable()){
-							this.read(key);
-						}
-						//9 写数据
-						if(key.isWritable()){
-							//this.write(key); //ssc
-						}
+						handleInput(key);
 					}
-					
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	private void accept(SelectionKey key) {
-		try {
-            System.out.println("accept方法...");
-            //1 获取服务通道
+
+	//处理事件
+	private void handleInput(SelectionKey key) throws IOException {
+		//7 如果为"OP_ACCEPT"事件,处理新接入的客户端的请求
+		if(key.isAcceptable()){
+			System.out.println("server端accept方法...");
+			//1 获取关心当前事件的Channel(服务端通道)
 			ServerSocketChannel ssc =  (ServerSocketChannel) key.channel();
-			//2 执行阻塞方法
+			//2 接受连接，获取一个SocketChannel
 			SocketChannel sc = ssc.accept();
-			//3 设置阻塞模式
+			//3 将SocketChannel设置为"非阻塞"模式
 			sc.configureBlocking(false);
-			//4 注册到多路复用器上，并设置读取标识
+			//4 SocketChannel注册到多路复用器上，并设置关注"OP_READ"事件
 			sc.register(this.seletor, SelectionKey.OP_READ);
-		} catch (IOException e) {
-			e.printStackTrace();
+		}
+		//8 如果为“OP_READ”事件,处理对端发送的数据
+		if(key.isReadable()){
+			System.out.println("server端read方法...");
+			//1 清空缓冲区旧的数据
+			this.readBuf.clear();
+			//2 获取之前注册的SocketChannel通道对象
+			SocketChannel sc = (SocketChannel) key.channel();
+			//3 从通道里读取数据，写入readBuf
+			int count = sc.read(this.readBuf);
+			if(count>0){
+				//5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
+				this.readBuf.flip();
+				//6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
+				byte[] bytes = new byte[this.readBuf.remaining()];
+				//7 接收缓冲区数据
+				this.readBuf.get(bytes);
+				//8 打印结果
+				String body = new String(bytes).trim();
+				System.out.println("Server端收到的数据 : " + body);
+				// 9..可以写回给客户端数据
+				returnToClient(sc);
+			}else{
+				key.cancel();
+				sc.close();
+			}
 		}
 	}
 
-    private void read(SelectionKey key) {
-        try {
-            System.out.println("read方法...");
-            //1 清空缓冲区旧的数据
-            this.readBuf.clear();
-            //2 获取之前注册的socket通道对象
-            SocketChannel sc = (SocketChannel) key.channel();
-            //3 读取数据
-            int count = sc.read(this.readBuf);
-            //4 如果没有数据
-            if(count == -1){
-                key.channel().close();
-                key.cancel();
-                return;
-            }
-            //5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
-            this.readBuf.flip();
-            //6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
-            byte[] bytes = new byte[this.readBuf.remaining()];
-            //7 接收缓冲区数据
-            this.readBuf.get(bytes);
-            //8 打印结果
-            String body = new String(bytes).trim();
-            System.out.println("Server : " + body);
-
-            // 9..可以写回给客户端数据
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void write(SelectionKey key){
-        //ServerSocketChannel ssc =  (ServerSocketChannel) key.channel();
-        //ssc.register(this.seletor, SelectionKey.OP_WRITE);
-    }
+    //响应客户端数据
+    private void returnToClient(SocketChannel sc) throws IOException {
+		String response = "服务端返回给客户端数据";
+		byte[] respbytes = response.getBytes();
+		ByteBuffer buffer = ByteBuffer.allocate(respbytes.length);
+		buffer.put(respbytes);
+		buffer.flip();
+		//从buffer中读取数据写入到SocketChannel中
+		sc.write(buffer);
+	}
 
 	public static void main(String[] args) {
-		
-		new Thread(new Server(8765)).start();;
+		new Thread(new Server(8765)).start();
 	}
-	
-	
 }
