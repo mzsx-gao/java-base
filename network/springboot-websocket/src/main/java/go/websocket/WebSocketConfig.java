9d9a1b624daea8e7efeadb9888850c04c70deeb9
mzsx-gao <18818196720@613.com>
2020-11-16 18:25:31 +0800

    springboot集成原始websocket实现聊天功能示例

--- network/springboot-websocket/src/main/java/go/websocket/WebSocketConfig.java
+++ network/springboot-websocket/src/main/java/go/websocket/WebSocketConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
- * @author Mark老师   享学课堂 https://enjoy.ke.qq.com
- * 往期课程和VIP课程咨询 依娜老师  QQ：2133576719
  * 类说明：开启WebSocket支持
  */
 @Configuration
--- network/springboot-websocket/src/main/java/go/websocket/WebSocketServer.java
+++ network/springboot-websocket/src/main/java/go/websocket/WebSocketServer.java
@@ -7,30 +7,27 @@ import org.springframework.stereotype.Component;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- *@author Mark老师   享学课堂 https://enjoy.ke.qq.com
- *往期课程和VIP课程咨询 依娜老师  QQ：2133576719
- *类说明：提供WebSocket服务
- */
-@ServerEndpoint(value = "/ws/asset")/*将类定义成一个WebSocket服务器端*/
+//将类定义成一个WebSocket服务器端
+@ServerEndpoint(value = "/ws/asset")
 @Component
 public class WebSocketServer {
 
     private static Logger log = LoggerFactory.getLogger(WebSocketServer.class);
-    private static final AtomicInteger onlineCount
-            = new AtomicInteger(0);
-    /*线程安全Set，用来存放每个客户端对应的Session对象。*/
-    private static CopyOnWriteArraySet<Session> sessionSet
-            = new CopyOnWriteArraySet<Session>();
-    /*线程安全Map，用来存放每个客户端sessionid和用户名的对应关系。*/
-    private static Map<String,String> sessionMap
-            = new ConcurrentHashMap<>();
+
+    private static final AtomicInteger onlineCount = new AtomicInteger(0);
+
+    //线程安全Set，用来存放每个客户端对应的Session对象
+    private static CopyOnWriteArraySet<Session> sessionSet = new CopyOnWriteArraySet<>();
+
+    //线程安全Map，用来存放每个客户端sessionid和用户名的对应关系
+    private static Map<String, String> sessionMap = new ConcurrentHashMap<>();
 
 
     /*** 连接建立成功调用的方法*/
@@ -38,18 +35,14 @@ public class WebSocketServer {
     public void onOpen(Session session) {
         //将用户session，session和用户名对应关系放入本地缓存
         sessionSet.add(session);
-        Map<String, List<String>> pathParameters
-                = session.getRequestParameterMap();
+        Map<String, List<String>> pathParameters = session.getRequestParameterMap();
         String userId = pathParameters.get("toUserId").get(0);
-        sessionMap.put(session.getId(),userId);
+        sessionMap.put(session.getId(), userId);
         log.info("有连接加入，当前连接数为：{}", onlineCount.incrementAndGet());
 
-        try {
-            //通知所有用户有新用户上线
-            broadCastInfo("系统消息@^用户["+userId+"]加入群聊。");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //通知所有用户有新用户上线
+        broadCastInfo("系统消息@^用户[" + userId + "]加入群聊。");
+
     }
 
     /*** 连接关闭调用的方法*/
@@ -57,43 +50,68 @@ public class WebSocketServer {
     public void onClose(Session session) {
         //将用户session，session和用户名对应关系从本地缓存移除
         sessionSet.remove(session);
-        Map<String, List<String>> pathParameters
-                = session.getRequestParameterMap();
+        Map<String, List<String>> pathParameters = session.getRequestParameterMap();
         String userId = sessionMap.get(session.getId());
         sessionMap.remove(session.getId());
         int cnt = onlineCount.decrementAndGet();
         log.info("有连接关闭，当前连接数为：{}", cnt);
-        try {
-            //通知所有用户有用户下线
-            broadCastInfo("系统消息@^用户["+userId+"]退出群聊。");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //通知所有用户有用户下线
+        broadCastInfo("系统消息@^用户[" + userId + "]退出群聊。");
+
     }
 
     /*** 收到客户端消息后调用的方法*/
     @OnMessage
     public void onMessage(String message, Session session) {
-        log.info("来自客户端{}的消息：{}",
-                sessionMap.get(session.getId()),message);
-        if(message.startsWith("ToUser:")){
-            //这里可以实现一对一聊天sendMessageAlone();
-        }else{
+        log.info("来自客户端{}的消息：{}", sessionMap.get(session.getId()), message);
+        if (message.startsWith("ToUser:")) {
+            //这里可以实现一对一聊天sendMessageAlone(),这里"Lison"写死了，实际上应该前端传过来
+            String sessionId="";
+            System.out.println("寻找单聊的人");
+            for (Map.Entry<String, String> m : sessionMap.entrySet()) {
+                System.out.println("key:" + m.getKey() + " value:" + m.getValue());
+                if(m.getValue().equals("Lison")){
+                    sessionId = m.getKey();
+                }
+            }
+            sendMessageAlone(sessionId, message);
+        } else {
             //实现群聊
             String msger = sessionMap.get(session.getId());
-            try {
-                broadCastInfo(msger+"@^"+message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            broadCastInfo(msger + "@^" + message);
         }
     }
 
     /***出现错误时的处理*/
     @OnError
     public void onError(Session session, Throwable error) {
-        log.error("发生错误：{}，Session ID： {}",error.getMessage(),session.getId());
+        log.error("发生错误：{}，Session ID： {}", error.getMessage(), session.getId());
         error.printStackTrace();
+    }
+
+    /*** 群发消息*/
+    public static void broadCastInfo(String message) {
+        for (Session session : sessionSet) {
+            if (session.isOpen()) {
+                basicSendMessage(session, message);
+            }
+        }
+    }
+
+    /*** 指定Session发送消息*/
+    public static void sendMessageAlone(String sessionId, String message) {
+        Session session = null;
+        for (Session s : sessionSet) {
+            if (s.getId().equals(sessionId)) {
+                session = s;
+                break;
+            }
+        }
+        if (session != null) {
+            basicSendMessage(session, message);
+        } else {
+            log.warn("没有找到你指定ID的会话：{}", sessionId);
+        }
     }
 
     /*** 发送消息的基础方法*/
@@ -105,31 +123,4 @@ public class WebSocketServer {
             e.printStackTrace();
         }
     }
-
-    /*** 群发消息*/
-    public static void broadCastInfo(String message) throws IOException {
-        for (Session session : sessionSet) {
-            if(session.isOpen()){
-                basicSendMessage(session, message);
-            }
-        }
-    }
-
-    /*** 指定Session发送消息*/
-    public static void sendMessageAlone(String sessionId, String message) throws IOException {
-        Session session = null;
-        for (Session s : sessionSet) {
-            if(s.getId().equals(sessionId)){
-                session = s;
-                break;
-            }
-        }
-        if(session!=null){
-            basicSendMessage(session, message);
-        }
-        else{
-            log.warn("没有找到你指定ID的会话：{}",sessionId);
-        }
-    }
-
 }
