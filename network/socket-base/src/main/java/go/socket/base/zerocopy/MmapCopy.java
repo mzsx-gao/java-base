7f8bd642b9e5d95897df451f1f3f8c1ee403fdf7
mzsx-gao <18818196720@613.com>
2022-01-12 16:33:35 +0800

    commit

--- network/socket-base/src/main/java/go/socket/base/zerocopy/MmapCopy.java
+++ network/socket-base/src/main/java/go/socket/base/zerocopy/MmapCopy.java
@@ -9,14 +9,15 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+
 /**
- * RocketMQ中的MMAP
+ * mmap零拷贝
  */
-public class MmapCopy
-{
-    public static String path = "D:\\DTLFolder";//需要内存映射的文件目录
-    public static void main( String[] args ) throws Exception
-    {
+public class MmapCopy {
+
+    public static String path = "需要内存映射的文件目录";
+
+    public static void main(String[] args) throws Exception {
         //映射的文件
         File file1 = new File(path, "1");
         //映射文件的fileChannel对象（操作文件，）
@@ -29,14 +30,14 @@ public class MmapCopy
         mmap.flip();
         byte[] bb = new byte[4];
         //从mmap中读取文件
-        mmap.get(bb,0,4);
+        mmap.get(bb, 0, 4);
         System.out.println(new String(bb));
         //解除MMAP
         unmap(mmap);
     }
 
     private static void unmap(MappedByteBuffer bb) {
-        Cleaner cl = ((DirectBuffer)bb).cleaner();
+        Cleaner cl = ((DirectBuffer) bb).cleaner();
         if (cl != null)
             cl.clean();
     }
